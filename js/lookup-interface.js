ef9322ba94a259d202ffbf92d5580d3a8445f60f
David Adeola <adeoladavid15@gmail.com>
2017-06-30 13:08:45 +0300

    This will add the starter code to the lookup-interface.js

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,8 +1,18 @@
+var apikey = require('./../env').apikey;
+
 $(document).ready(function(){
   $('form').submit(function(event){
     event.preventDefault();
     var userName = $('#userName').val();
 
-    console.log(userName);
+    // console.log(userName);
+
+    exports.getRepos = function(){
+      $.get('https://api.github.com/users/daneden?access_token=' + apikey).then(function(response){
+        console.log(response);
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+    };
   });
 });
